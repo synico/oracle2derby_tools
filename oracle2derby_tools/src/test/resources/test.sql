d3d880e9a575cf275f4a7c9f9aaf942d2885d82e
synico <synico@gmail.com>
2016-10-12 17:07:55 +0800

    add derby client info

---
+++ oracle2derby_tools/src/test/resources/test.sql
@@ -0,0 +1,7 @@
+Name: network_derby
+Driver: Apache Derby Client
+URL: jdbc:derby://localhost:1527/D:\IBM\WCDE_ENT70\db\mall_network;create=true
+UserName: APP
+Password: APP
+
+jdbc:derby:D:\IBM\WCDE_ENT70\db\mall_empty
